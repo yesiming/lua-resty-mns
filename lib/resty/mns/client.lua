39fe976c1e0bc1d775cd6838c4c93b10da9e80c1
nason <zyxtomcat@gmail.com>
2017-02-16 23:50:30 +0700

    New file commit

---
+++ lib/resty/mns/client.lua
@@ -0,0 +1,32 @@
+local setmetatable = setmetatable
+
+local _M = {}
+
+local ok, new_tab = pcall(require, "table.new")
+if not ok then
+    new_tab = function (narr, nrec) return {} end
+end
+
+function _M:new( endpoint, accessid, accesskey, ststoken, connPoolSize = 200, timeout = 35, connectTimeout = 35)
+	local instance = {
+		endpoint = endpoint,
+		accessid = accessid,
+		accesskey = accesskey,
+		ststoken = ststoken,
+		connPoolSize = connPoolSize,
+		timeout = timeout,
+		connectTimeout = connPoolSize
+	}
+
+	return setmetatable( instance, {__index == _})
+end
+
+function _M:updateAccessId( accessid, accesskey, ststoken )
+	if not accessid then self.accessid = accessid end
+	if not accesskey then self.accesskey = accesskey end
+	if not ststoken then self.ststoken = ststoken end
+end
+
+function _M:getAccountAttributes()
+	
+end
---
+++ lib/resty/mns/utils/reponse.lua
@@ -0,0 +1,11 @@
+local setmetatable = setmetatable
+
+local _M = {}
+
+local mt = { __index = _M }
+
+function _M:GetAccountAttributesResponse( ... )
+	-- body
+end
+
+return _M
